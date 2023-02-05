3f314c99b20f6720743d0adcf8906acedf3a89b5
Kikoego <97868051+Kikoego@users.noreply.github.com>
2023-02-06 04:47:05 +0700

    Add files via upload

---
+++ weapon/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unit u1 = new Unit("Воин (норм чел)", 22, 120);
+            Unit u2 = new Unit("Маг (лох)", 20, 100);
+
+            while (true)
+            {
+                if (u1.Attack(u2) == false || u2.Attack(u1) == false) break;
+            };
+
+
+            u1.Display();
+            u2.Display();
+
+        }
+    }
+}
